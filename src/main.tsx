cd165ee222e2b05e0c9b75ab542f7aae7c4d83d7
henrysallan <isthishenry@gmail.com>
2025-07-20 23:02:55 -0400

    fix css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'  // This line is crucial!
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
